3f4241f494b3213acde20099f1dd00120b80968f
Volodymyr Buinov <buinoff@gmail.com>
2023-01-22 11:47:44 +0200

    Usage examples and tests results moved to REDME.md file

--- creational/singletone/implementing.php
+++ creational/singletone/implementing.php
@@ -12,7 +12,7 @@ final class Db
         $connectionString = $settings['driver'] . ':host=' . $settings['host'] . ';port=' . $settings['db_port'] . ';dbname=' . $settings['db_name'];
 
         try {
-            // Connect to database.
+            // Connect to database. Late Static Bindings
             static::$_connection = new PDO($connectionString, $settings['username'], $settings['password'], $settings['options']);
             static::$_connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             static::$_connection->query('SET NAMES utf8');
@@ -25,7 +25,7 @@ final class Db
     public static function getInstance(): self
     {
         if (!static::$_instance) {
-            static::$_instance = new static();
+            static::$_instance = new static(); // Late Static Bindings
         }
 
         return static::$_instance;
@@ -44,28 +44,3 @@ final class Db
     {
     }
 }
-
-// Tests
-
-// $stmt = new Db(); // Fatal error: Uncaught Error: Call to private Db::__construct() from global scope
-$db_instance = Db::getInstance();
-$db_connection = $db_instance->getConnection();
-var_dump($db_instance); // class Db#1 (0) { }
-var_dump($db_connection); // class PDO#2 (0) { }
-$db_instance2 = Db::getInstance();
-$db_connection2 = $db_instance2->getConnection();
-var_dump($db_instance2); // class Db#1 (0) { }
-var_dump($db_connection2); // class PDO#2 (0) { }
-
-$sqlExample = 'SELECT * FROM `user`';
-try {
-    $stm = $db_connection->prepare($sqlExample);
-    $stm->execute();
-} catch (\PDOException $e) {
-    die("PDO Error: " . $e->getMessage());
-}
-
-var_dump($stm->fetchAll(PDO::FETCH_ASSOC)); // array()
-var_dump($db_connection->errorInfo()); // array()
-var_dump($db_instance === $db_instance2); // true
-var_dump($db_connection === $db_connection2); // true
