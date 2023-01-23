ff87e93695b7ec2040a53d2f2473d949eb53d9b5
Volodymyr Buinov <buinoff@gmail.com>
2023-01-23 16:52:11 +0200

    Created Static Factory implementaition and README.md with usage example

--- creational/static_factory/implementing.php
+++ creational/static_factory/implementing.php
@@ -1,2 +1,34 @@
 <?php
 
+interface Food
+{
+    public function made();
+}
+
+class Pizza implements Food
+{
+    public function made(): string
+    {
+        return 'I`m a Pizza!';
+    }
+}
+
+class Sushi implements Food
+{
+    public function made(): string
+    {
+        return 'I`m a Sushi!';
+    }
+}
+
+class FoodFactory
+{
+    public static function make($foodTitle): Food|Exception
+    {
+        $ClassName = ucfirst($foodTitle);
+        if (class_exists($ClassName)) {
+            return new $ClassName();
+        }
+        return throw new Exception("Error making Food", 1,);
+    }
+}
