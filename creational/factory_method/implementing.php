7f96e87aea1f8349aad838efde06b442b23519a1
Volodymyr Buinov <buinoff@gmail.com>
2023-01-23 14:55:59 +0200

    Created Factory Method implementaition and README.md with usage example

--- creational/factory_method/implementing.php
+++ creational/factory_method/implementing.php
@@ -1,2 +1,43 @@
 <?php
 
+interface Vehicle
+{
+    public function made();
+}
+
+class Car implements Vehicle
+{
+    public function made(): string
+    {
+        return 'I`m a Car';
+    }
+}
+
+class Motorbike implements Vehicle
+{
+    public function made(): string
+    {
+        return 'I`m a Motorbike';
+    }
+}
+
+interface VehicleFactory
+{
+    public static function made();
+}
+
+class CarFactory implements VehicleFactory
+{
+    public static function made()
+    {
+        return new Car();
+    }
+}
+
+class MotorbikeFactory implements VehicleFactory
+{
+    public static function made()
+    {
+        return new Motorbike();
+    }
+}
